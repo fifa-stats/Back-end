e98b47cf26ca09d995afb420fbfa4688bec22726
Vu Cao <cdhvu@yahoo.com>
2019-03-11 10:53:44 -0700

    Adding test route

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+// @route GET /test
+// @desc Test server
+// @access Public
+app.get("/test", (req, res) => {
+  res.status(200).json({ message: "success" });
+});
+
 app.listen(5000, () => console.log("App is running on port 5000"));
