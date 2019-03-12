3ff1b34b22c2a9b3421a10069ab45847ebcb458a
Vu Cao <cdhvu@yahoo.com>
2019-03-11 22:40:24 -0700

    Adding corse and helmet to server

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const users = require("./api/users");
 
 const app = express();
 
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5000"
+};
+
 //middlewares
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(helmet());
 
 //Use Routes
 app.use("/api/users", users);
