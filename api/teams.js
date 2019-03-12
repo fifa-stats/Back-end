7e7eb0debf55f3defd5cc63f08dd6544a754e1be
Vu Cao <cdhvu@yahoo.com>
2019-03-12 11:22:18 -0700

    Creating POST endpoint to add new team for current user

--- api/teams.js
+++ api/teams.js
@@ -16,7 +16,25 @@ router.get(
       const teams = await db.findTeams(userId);
       res.status(200).json(teams);
     } catch (error) {
-      res.status(400).json({ error: error.message });
+      res.status(400).json({ error: "Failed to fetch teams" });
+    }
+  }
+);
+
+// @route POST api/teams/
+// @desc Create new team for current user
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const newTeam = await db.createTeam(req.body.name, req.user.id);
+      res.status(201).json(newTeam[0]);
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: "Failed to create team", error: error.message });
     }
   }
 );
