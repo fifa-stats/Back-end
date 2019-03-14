011e9483ad376e9998531801ad826762bcf053b0
Vu Cao <cdhvu@yahoo.com>
2019-03-14 14:49:42 -0700

    Updating test pythonscript

--- script/pythonscript.py
+++ script/pythonscript.py
@@ -1,11 +1,12 @@
-import sys, json, numpy as np
+import sys, json, pandas, numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 def main():
-    parsedLine = json.loads(sys.stdin.readlines()[0])
-    result = [{'id': i['id'], 'name': i['Name']} for i in parsedLine]
-    return json.dumps(result)
+   parsedLine = json.loads(sys.stdin.readlines()[0])
+   result = [{'id': i[  'id'], 'name': i['Name']} for i in parsedLine]
+   print(json.dumps(result)) 
+   sys.stdout.flush()
 
 #start process
 if __name__ == '__main__':
-    processedData = main()
-    print(processedData)
+    main()
